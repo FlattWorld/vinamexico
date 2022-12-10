2cc26d56db3da1196fdd581b6a41a1abf4866c92
Lino09 <onosmx@gmail.com>
2022-12-09 19:18:56 -0600

    chore: add translations

--- ui/components/Home/MisionYVision.tsx
+++ ui/components/Home/MisionYVision.tsx
@@ -8,16 +8,16 @@ const MisionYVision = () => {
   const lang: 'EN' | 'ES' = useContext(LanguageContext) as 'EN' | 'ES';
   return (
     <div className="flex flex-col items-center">
-      <h2 className="theme-title">Nuestra visión y misión</h2>
+      <h2 className="theme-title">{MisionAndVision[lang].title}</h2>
       <div className="flex gap-8 w-10/12">
         <Image src={hands} alt="hands-praying" className="w-1/3 border" />
         <div className="w-2/3 flex flex-col gap-8 justify-center">
           <p>
-            <span>{MisionAndVision[lang][0]}</span>
-            <span className="keyword">{MisionAndVision[lang][1]}</span>
-            <span>{MisionAndVision[lang][2]}</span>
+            <span>{MisionAndVision[lang].texts[0]}</span>
+            <span className="keyword">{MisionAndVision[lang].texts[1]}</span>
+            <span>{MisionAndVision[lang].texts[2]}</span>
           </p>
-          <p>{MisionAndVision[lang][3]}</p>
+          <p>{MisionAndVision[lang].texts[3]}</p>
         </div>
       </div>
     </div>
